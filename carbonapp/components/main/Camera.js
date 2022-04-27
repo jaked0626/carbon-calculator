48c6c366fdc9a378e7ce8607300e6b553838915e
jaked0626 <jakez0626@gmail.com>
2022-04-27 22:23:33 +0900

    added camera capture button

--- carbonapp/components/main/Camera.js
+++ carbonapp/components/main/Camera.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
-import { Camera } from 'expo-camera';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Alert, Image } from 'react-native';
+import { Camera, CameraPreview } from 'expo-camera';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [previewVisible, setPreviewVisible] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -13,6 +16,19 @@ export default function App() {
       setHasPermission(status === 'granted');
     })();
   }, []);
+
+  const takePicture = async () => {
+    const {status} = await Camera.requestCameraPermissionsAsync();
+    if (status === 'granted') {
+      if (camera) { // if camera cannot be found, don't take picture. 
+        const photoData = await camera.takePictureAsync({quality: 1,});
+        setImage(photoData);
+        setPreviewVisible(true);
+      }
+    } else {
+      Alert.alert("Needs permission to use camera")
+    }
+  };
 
   if (hasPermission === null) {
     return <View />;
@@ -22,19 +38,28 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
+      <Camera 
+        style={styles.camera} 
+        type={type}
+        ref={ref => setCamera(ref)} // assigns camera variable to the camera we are using
+        > 
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={styles.button}
             onPress={() => {
               setType(
-                type === Camera.Constants.Type.back
+                type === Camera.Constants.Type.back // if camera type is back, change to front, otherwise change to back
                   ? Camera.Constants.Type.front
                   : Camera.Constants.Type.back
               );
             }}>
             <MaterialCommunityIcons name = 'rotate-3d-variant' color = '#A3E4D7' size = {45} />
           </TouchableOpacity>
+        </View>
+        <View style = {styles.cameraButtonContainer}>
+        <TouchableOpacity
+            style={styles.cameraButton}
+            onPress={() => { takePicture();}}/>
         </View>
       </Camera>
     </View>
@@ -60,6 +85,24 @@ const styles = StyleSheet.create({
     top : 15,
     right: -15,
   },
+  cameraButton: {
+    //flex: 0.23,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: 90,
+    height: 90,
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: '#A3E4D7',
+    shadowOpacity: 1,
+    shadowRadius: 1,
+    top: 80,
+  },
+  cameraButtonContainer: {
+    flex: 1,
+    flexDirection: 'column-reverse'
+  },
   text: {
     fontSize: 18,
     marginBottom: 20,
@@ -67,5 +110,8 @@ const styles = StyleSheet.create({
     marginRight: -15,
     marginTop: 10,
     color: 'white',
+  },
+  image: {
+    flex: 1,
   },
  }); 
